f1608097f5e5deccb89062b1b72ba69b2da929f7
Matthew Knowles <matthewyknowles@gmail.com>
2017-08-15 18:31:58 -0400

    basic roman numeral setup

---
+++ spec/romannumeral.spec.js
@@ -0,0 +1,5 @@
+describe("Roman Numerals", () => {
+    it("should return I for 1", () => {
+        expect(toRomanNumeral(1)).toEqual("I");
+    })
+})
---
+++ src/romannumeral.js
@@ -0,0 +1,3 @@
+function toRomanNumeral(toConvert) {
+    return "";
+}
